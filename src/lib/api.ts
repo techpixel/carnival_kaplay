1490b01db115bd2ebc185aef538842b4a400dba0
techpixel <68567672+techpixel@users.noreply.github.com>
2025-03-18 16:45:17 -0500

    remove auth headers

--- src/lib/api.ts
+++ src/lib/api.ts
@@ -1,5 +1,3 @@
-const AUTH_TOKEN = "mvNDH1nGMhJixVtdDhHJeUa5TAJGCx5B";
-
 export type JokerData = {
     id: string;
     instance_id: string;
@@ -14,9 +12,6 @@ export type JokerData = {
 async function drawHand(): Promise<JokerData[]> {
     return fetch("/draw-hand", {
         method: "POST",
-        headers: {
-            "Authorization": `Bearer ${AUTH_TOKEN}`
-        }
     })
         .then((response) => response.json())
         .catch((error) => console.error(error));
@@ -25,9 +20,6 @@ async function drawHand(): Promise<JokerData[]> {
 async function currentHand(): Promise<JokerData[]> {
     return fetch("/hand", {
         method: "POST",
-        headers: {
-            "Authorization": `Bearer ${AUTH_TOKEN}`
-        }
     })
         .then((response) => response.json())
         .catch((error) => console.error(error));
@@ -62,7 +54,6 @@ async function discard(indexes: number[]): Promise<JokerData[]> {
     return fetch("/redraw", {
         method: "POST",
         headers: {
-            "Authorization": `Bearer ${AUTH_TOKEN}`,
             "Content-Type": "application/json"
         },
         body: JSON.stringify(indexes)
